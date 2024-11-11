3e2d148156f082fbc673804f2931d4f39f708049
Davlatov Rustam <davlatov_rm@mail.ru>
2024-11-11 20:18:46 +0300

    SQL

--- app/models/task.py
+++ app/models/task.py
@@ -1,11 +1,14 @@
 from app.backend.db import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import CreateTable
+from app.models import user
+from slugify import slugify
 
+#
 
 class Task(Base):  # модель Task, наследованную от ранее написанного Base
     __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
 
     title = Column(String)
@@ -13,8 +16,11 @@ class Task(Base):  # модель Task, наследованную от ране
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)  # внешний ключ на id из таблицы 'users'
-
+    slug = Column(String)
     user = relationship('User', back_populates='tasks')  # объект связи с таблицей User
 
 
+
+from sqlalchemy.schema import CreateTable
 print(CreateTable(Task.__table__))
+
